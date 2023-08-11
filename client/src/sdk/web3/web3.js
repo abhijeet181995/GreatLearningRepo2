18fb4dde6f5d64472fefdae2710fd2ce5a633301
Abhijeet Srivastava <abhijeet181995@gmail.com>
2023-08-11 10:09:34 +0530

    setting env

--- client/src/sdk/web3/web3.js
+++ client/src/sdk/web3/web3.js
@@ -9,7 +9,7 @@ export const getWeb3 = async () => {
   } else {
     //const provider = new Web3.providers.HttpProvider("http://127.0.0.1:7545");
     //Update Alchemy API_URL in client/.env
-     const provider = new Web3.providers.HttpProvider("https://eth-sepolia.g.alchemy.com/v2/GOKFTMlY0P9Po6Eu-9YFlPTkB87o0VUS");
+     const provider = new Web3.providers.HttpProvider(process.env.API_URL);
     return new Web3(provider);
   }
 };
